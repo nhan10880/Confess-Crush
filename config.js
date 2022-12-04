d22689f4c1672d853040955322c79685ddb691de
nhan10880 <62728122+nhan10880@users.noreply.github.com>
2022-12-04 12:52:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 3h, anh qua đón đi chơi nha.',
+    messDesc: 'Chiều nay 3h, anh qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100016039778625'
 }
